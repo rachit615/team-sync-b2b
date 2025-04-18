0fd443c4be0427e1fed59012880341dd97a2c170
rachit615 <rachitjain615@gmail.com>
2025-04-19 00:53:19 +0530

    add custom error handling with exception classes

--- backend/src/middlewares/errorHandler.middleware.ts
+++ backend/src/middlewares/errorHandler.middleware.ts
@@ -1,5 +1,6 @@
 import { ErrorRequestHandler } from "express";
 import { HTTP_STATUS } from "../config/http.config";
+import { AppError } from "../utils/appError";
 
 export const errorHandler: ErrorRequestHandler = (err, req, res, next): any => {
   console.error(`Error occured on PATH: ${req.path} `, err);
@@ -8,6 +9,12 @@ export const errorHandler: ErrorRequestHandler = (err, req, res, next): any => {
     return res
       .status(HTTP_STATUS.BAD_REQUEST)
       .json({ message: "Invalid JSON format. Please check your request." });
+  }
+
+  if (err instanceof AppError) {
+    return res
+      .status(err.statusCode)
+      .json({ message: err.message, errorCode: err.errorCode });
   }
   return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
     message: "Internal Server Error",
