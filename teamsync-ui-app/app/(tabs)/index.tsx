3d0de50c92eb548d537ab9a579e8101020af5e89
rachit615 <rachitjain615@gmail.com>
2025-08-13 01:03:21 +0530

    handle persistent login

--- teamsync-ui-app/app/(tabs)/index.tsx
+++ teamsync-ui-app/app/(tabs)/index.tsx
@@ -1,11 +1,40 @@
 import Button from "@/components/Common/Button/Button";
 import { router } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useState } from "react";
 
 export default function HomeScreen() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      try {
+        const token = await AsyncStorage.getItem("token");
+        if (token) {
+          router.replace("/(workspace)/workspace");
+        }
+      } catch (err) {
+        console.error("Error checking token:", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkLoginStatus();
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" color="#006EE9" />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>Team Sync </Text>
+      <Text style={styles.heading}>Team Sync</Text>
       <Button
         BtnText={"Get Started"}
         onButtonClick={() => {
@@ -29,7 +58,7 @@ const styles = StyleSheet.create({
     fontSize: 30,
     marginTop: 20,
     color: "#006EE9",
-    fontWeight: 600,
+    fontWeight: "600",
   },
   buttonGS: {
     position: "absolute",
